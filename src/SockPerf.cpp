bf6df5fda32192a8ec15903c1d531fd76c7568a9
yeelas@mellanox.com <yeelas@mellanox.com@afdf6bed-98c4-f77c-2062-c9886063b274>
2011-12-05 11:51:58 +0000

    Changed tcp-avoid-nodelay description.

--- src/SockPerf.cpp
+++ src/SockPerf.cpp
@@ -182,7 +182,7 @@ static const AOPT_DESC  common_opt_desc[] =
 	},
 	{
 		OPT_TCP_NODELAY_OFF, AOPT_NOARG, aopt_set_literal( 0 ), aopt_set_string( "tcp-avoid-nodelay" ),
-             "tcp-nodelay uses for delivering TCP Messages Immediately (default ON)."
+             Stop delivering TCP Messages Immediately (Enable Nagel)."
 	},
 	{
 		OPT_NONBLOCKED_SEND, AOPT_NOARG, aopt_set_literal( 0 ), aopt_set_string( "tcp-skip-blocking-send" ),
