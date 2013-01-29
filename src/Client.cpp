fac0e0bcb6902b5d2f15638ee150dc4a62068f5d
rosenbaumalex@gmail.com <rosenbaumalex@gmail.com@afdf6bed-98c4-f77c-2062-c9886063b274>
2013-01-29 08:21:56 +0000

    Support loopback connections - local network connection between server & client on same machine Fixes: http://code.google.com/p/sockperf/issues/detail?id=40
    Now bind() on client socket will happen only if sockperf flag '--client_port' is specified for all socket types TCP, UDP UC + MC.
    Until now client used to bind(ADDRANY:server_port) only for UDP sockets.
    
    (not changed)
    -	Loopback is disabled in socket by default. Need to use '--mc-loopback-enable' to allow MC loopback traffic (both server + client)
    -	Default for '--client_port' is 0, which means don't bind.
    -	Only TCP (SOCK_STREAM) calls connect() to server

--- src/Client.cpp
+++ src/Client.cpp
@@ -471,38 +471,35 @@ int Client<IoType, SwitchDataIntegrity, SwitchActivityInfo, SwitchCycleDuration,
 	/* bind/connect socket */
 	if (rc == SOCKPERF_ERR_NONE)
 	{
-		struct sockaddr_in bind_addr;
+		struct sockaddr_in saddr;
 
 		// cycle through all set fds in the array (with wrap around to beginning)
 		for (int ifd = m_ioHandler.m_fd_min; ifd <= m_ioHandler.m_fd_max; ifd++) {
 
 			if (!(g_fds_array[ifd] && (g_fds_array[ifd]->active_fd_list))) continue;
 
-			memset(&bind_addr, 0, sizeof(struct sockaddr_in));
+			memset(&saddr, 0, sizeof(struct sockaddr_in));
 
-			if (g_fds_array[ifd]->sock_type == SOCK_DGRAM) {
-				bind_addr.sin_family = AF_INET;
-				if (g_pApp->m_const_params.client_port)
-				{
-					bind_addr.sin_port = g_pApp->m_const_params.client_port;
-				}
-				else
-				{
-					bind_addr.sin_port = g_fds_array[ifd]->addr.sin_port;
-				}
-				bind_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-				log_dbg ("binding to: %s:%d [%d]...", inet_ntoa(bind_addr.sin_addr), ntohs(bind_addr.sin_port), ifd);
-				if (bind(ifd, (struct sockaddr*)&bind_addr, sizeof(struct sockaddr)) < 0) {
-					log_err("Can`t bind socket %s:%d [%d].", inet_ntoa(bind_addr.sin_addr), ntohs(bind_addr.sin_port), ifd);
+			saddr.sin_family = AF_INET;
+			if (g_pApp->m_const_params.client_port) {
+				saddr.sin_port = g_pApp->m_const_params.client_port;
+				saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+				log_dbg ("[fd=%d] Binding to: %s:%d...", ifd, inet_ntoa(saddr.sin_addr), ntohs(saddr.sin_port));
+				if (bind(ifd, (struct sockaddr*)&saddr, sizeof(struct sockaddr)) < 0) {
+					log_err("[fd=%d] Can`t bind socket %s:%d", ifd, inet_ntoa(saddr.sin_addr), ntohs(saddr.sin_port));
 					rc = SOCKPERF_ERR_SOCKET;
 					break;
 				}
 			}
 			else {
-				memcpy(&bind_addr, &(g_fds_array[ifd]->addr), sizeof(struct sockaddr_in));
-				log_dbg ("connecting to: %s:%d [%d]...", inet_ntoa(bind_addr.sin_addr), ntohs(bind_addr.sin_port), ifd);
-				if (connect(ifd, (struct sockaddr*)&bind_addr, sizeof(struct sockaddr)) < 0) {
-					if(errno == EINPROGRESS) {
+				log_dbg ("[fd=%d] binding to: %s:%d...", ifd, inet_ntoa(saddr.sin_addr), ntohs(saddr.sin_port));
+			}
+
+			if (g_fds_array[ifd]->sock_type == SOCK_STREAM) {
+				memcpy(&saddr, &(g_fds_array[ifd]->addr), sizeof(struct sockaddr_in));
+				log_dbg ("[fd=%d] Connecting to: %s:%d...", ifd, inet_ntoa(saddr.sin_addr), ntohs(saddr.sin_port));
+				if (connect(ifd, (struct sockaddr*)&saddr, sizeof(struct sockaddr)) < 0) {
+					if (errno == EINPROGRESS) {
 						fd_set rfds, wfds;
 						struct timeval tv;
 						tv.tv_sec = 0; tv.tv_usec = 500;
