811a3866a8a9dfbfe186cd1975dcbda5f6b6e6b2
Michal Schmidt <mschmidt@redhat.com>
2015-05-20 16:10:47 +0200

    autogen.sh: allow running it from another directory

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -ex
+
+oldpwd=$(pwd)
+topdir=$(dirname "$0")
+cd "$topdir"
+
 rm -rf autom4te.cache
 mkdir -p config/m4 config/aux
 autoreconf -v --install || exit 1
 rm -rf autom4te.cache
- 
+
+cd "$oldpwd"
+
 exit 0
