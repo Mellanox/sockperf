f2b51fe45ad054be1f517b33245fb7f5944a9063
Avner BenHanoch <avnerb@mellanox.com>
2015-10-06 13:32:14 +0200

    support rpm _topdir argument; also support easy building other projects' rpms

--- build/build-git-rpm.sh
+++ build/build-git-rpm.sh
@@ -5,18 +5,25 @@ script_dir=`dirname $(readlink -f $0)`
 cd $script_dir/..
 
 source ./build/versioning.sh
-sockperf_dir=sockperf-$GIT_REF
-rm -rf /tmp/$sockperf_dir
-mkdir /tmp/$sockperf_dir
-cp -a * /tmp/$sockperf_dir/ # do not copy hidden files like .git
+APP_NAME=sockperf
+APP_NAME_GIT=$APP_NAME-$GIT_REF
+APP_VERSION=${VERSION}-${VER_GIT}
+APP_SPEC=./build/$APP_NAME-$APP_VERSION.spec
 
-cd /tmp/$sockperf_dir
-echo ${VERSION}-${VER_GIT} > ./build/current-version
+rm -rf /tmp/$APP_NAME_GIT
+mkdir /tmp/$APP_NAME_GIT
+cp -a * /tmp/$APP_NAME_GIT/ # do not copy hidden files like .git
+
+cd /tmp/$APP_NAME_GIT
+echo $APP_VERSION > ./build/current-version
 echo $GIT_REF >> ./build/current-version
-sed -e s/__GIT_REF__/$GIT_REF/g -e s/__VERSION__/$VERSION/g -e s/__VER_GIT__/$VER_GIT/g ./build/sockperf.spec.in > ./build/sockperf.spec
+sed -e s/__GIT_REF__/$GIT_REF/g -e s/__VERSION__/$VERSION/g -e s/__VER_GIT__/$VER_GIT/g ./build/$APP_NAME.spec.in > $APP_SPEC
 
-tar -zcf ../sockperf-$GIT_REF.tar.gz --exclude .git  -C .. $sockperf_dir
-mv ../sockperf-$GIT_REF.tar.gz ~/rpmbuild/SOURCES/ # TEMP
-
-rpmbuild -ba --rmsource --rmspec ./build/sockperf.spec
+if [ $# -lt 1 ]; then
+        RPM_DIR=`rpm --eval '%{_topdir}'`
+else
+        RPM_DIR=$1
+fi
+tar -zcf $RPM_DIR/SOURCES/$APP_NAME-$GIT_REF.tar.gz --exclude .git  -C .. $APP_NAME_GIT
+rpmbuild -ba --rmsource --rmspec --define "_topdir $RPM_DIR" $APP_SPEC
 cd $base_dir
