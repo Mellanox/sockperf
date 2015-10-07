357810709bb9b6d02b1e209e18ccbf07a9f307c9
Avner BenHanoch <avnerb@mellanox.com>
2015-10-07 13:02:48 +0200

    new code in comment just for easy tests

--- build/versioning.sh
+++ build/versioning.sh
@@ -5,3 +5,6 @@ GIT_REF=`git rev-parse HEAD 2> /dev/null || echo ""`
 x=`git describe --long --abbrev=12 --dirty 2> /dev/null || echo ""`
 if [ -n "$x" ]; then x=`echo $x | sed -e 's/-g/.git/' -e 's/-dirty/.dirty/' | sed s/.*-//`; else  x="no.git"; fi
 VER_GIT=$x
+VER_GIT=20 # TEMP !!!
+GIT_REF=${VERSION}-${VER_GIT} # TEMP !!!
+
