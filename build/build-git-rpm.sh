265d6ca1218bc2104027fac5c5171f0049d479f3
Avner BenHanoch <avnerb@mellanox.com>
2015-10-07 13:02:16 +0200

    remove temp directory

--- build/build-git-rpm.sh
+++ build/build-git-rpm.sh
@@ -9,12 +9,12 @@ APP_NAME=sockperf
 APP_NAME_GIT=$APP_NAME-$GIT_REF
 APP_VERSION=${VERSION}-${VER_GIT}
 APP_SPEC=./build/$APP_NAME-$APP_VERSION.spec
+TEMP_DIR=/tmp/$APP_NAME_GIT
+rm -rf $TEMP_DIR
+mkdir $TEMP_DIR
+cp -a * $TEMP_DIR/ # do not copy hidden files like .git
 
-rm -rf /tmp/$APP_NAME_GIT
-mkdir /tmp/$APP_NAME_GIT
-cp -a * /tmp/$APP_NAME_GIT/ # do not copy hidden files like .git
-
-cd /tmp/$APP_NAME_GIT
+cd $TEMP_DIR
 echo $APP_VERSION > ./build/current-version
 echo $GIT_REF >> ./build/current-version
 sed -e s/__GIT_REF__/$GIT_REF/g -e s/__VERSION__/$VERSION/g -e s/__VER_GIT__/$VER_GIT/g ./build/$APP_NAME.spec.in > $APP_SPEC
@@ -27,3 +27,4 @@ fi
 tar -zcf $RPM_DIR/SOURCES/$APP_NAME_GIT.tar.gz --exclude .git  -C .. $APP_NAME_GIT
 rpmbuild -ba --rmsource --rmspec --define "_topdir $RPM_DIR" $APP_SPEC
 cd $base_dir
+rm -rf $TEMP_DIR
