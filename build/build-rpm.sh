305790323efe254d03ad20012d75d20123d38e24
Avner BenHanoch <avnerb@mellanox.com>
2015-10-08 12:58:25 +0200

    rename field for matching convention we used in debian

--- build/build-rpm.sh
+++ build/build-rpm.sh
@@ -7,7 +7,7 @@ cd $script_dir/..
 source ./build/versioning.sh
 APP_NAME=sockperf
 APP_NAME_GIT=$APP_NAME-$GIT_REF
-APP_VERSION=${VERSION}-${VER_GIT}
+APP_VERSION=${VERSION}-${RELEASE}
 TEMP_DIR=/tmp/$APP_NAME_GIT
 rm -rf $TEMP_DIR
 mkdir $TEMP_DIR
@@ -23,7 +23,7 @@ else
         RPM_DIR=$1
 fi
 APP_SPEC=$RPM_DIR/SPECS/$APP_NAME-$APP_VERSION.spec
-sed -e s/__GIT_REF__/$GIT_REF/g -e s/__VERSION__/$VERSION/g -e s/__VER_GIT__/$VER_GIT/g ./build/$APP_NAME.spec.in > $APP_SPEC
+sed -e s/__GIT_REF__/$GIT_REF/g -e s/__VERSION__/$VERSION/g -e s/__RELEASE__/$RELEASE/g ./build/$APP_NAME.spec.in > $APP_SPEC
 
 tar -zcf $RPM_DIR/SOURCES/$APP_NAME_GIT.tar.gz --exclude .git  -C .. $APP_NAME_GIT
 rpmbuild -bs --define 'dist %{nil}' --define '_source_filedigest_algorithm md5' --define '_binary_filedigest_algorithm md5' --rmsource --rmspec --define "_topdir $RPM_DIR" $APP_SPEC
--- build/versioning.sh
+++ build/versioning.sh
@@ -4,7 +4,7 @@ VERSION=`cat version | tr -d '\n'`
 GIT_REF=`git rev-parse HEAD 2> /dev/null || echo "no.git"`
 x=`git describe --long --abbrev=12 --dirty 2> /dev/null || echo ""`
 if [ -n "$x" ]; then x=`echo $x | sed -e 's/-g/.git/' -e 's/-dirty/.dirty/' | sed s/.*-//`; else  x="no.git"; fi
-VER_GIT=$x
-#VER_GIT=20 # TEMP !!!
-#GIT_REF=${VERSION}-${VER_GIT} # TEMP !!!
+RELEASE=$x
+#RELEASE=20 # TEMP !!!
+#GIT_REF=${VERSION}-${RELEASE} # TEMP !!!
 
