5be4647b812105085eeab783a14370efdccb5c5d
Avner BenHanoch <avnerb@mellanox.com>
2015-10-11 08:49:32 +0200

    use dpkg-buildpackage instead of debuild so our build won't need installing anything

--- build/build-rpm-to-deb.sh
+++ build/build-rpm-to-deb.sh
@@ -27,7 +27,7 @@ ln -s $OLD_NAME$TARBALL_EXT ${APP_NAME}_${VERSION}.orig$TARBALL_EXT
 tar xf $OLD_NAME$TARBALL_EXT
 cd $OLD_NAME
 
-debuild -us -uc # actual build of *.deb
+dpkg-buildpackage -us -uc # actual build of *.deb
 cd $BASE_DIR
 cp --verbose $TEMP_DIR/*.deb .
 rm -rf $TEMP_DIR
