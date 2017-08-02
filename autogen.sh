c6a0d0e3ab532c3223f269561f07038892e5f421
Igor Ivanov <igor.ivanov.va@gmail.com>
2017-08-02 22:20:24 +0300

    Fix version output during compilation from tar
    This change allows to display valid version information in
    case the tool is built from sources (w/o git).
    
    Signed-off-by: Igor Ivanov <igor.ivanov.va@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -6,12 +6,9 @@ topdir=$(dirname "$0")
 cd "$topdir"
 
 CURRENT_VERSION_FILE=./build/current-version
-if [ -d .git ] # don't ruin file that might come from build-rpm.sh
-then
-	. ./build/versioning.sh
-	echo ${VERSION}-${RELEASE} > $CURRENT_VERSION_FILE
-	echo $GIT_REF >> $CURRENT_VERSION_FILE
-fi
+. ./build/versioning.sh
+echo ${VERSION}-${RELEASE} > $CURRENT_VERSION_FILE
+echo $GIT_REF >> $CURRENT_VERSION_FILE
 
 rm -rf autom4te.cache
 mkdir -p config/m4 config/aux
