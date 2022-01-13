e3a9893af9e097b68fa2f6d1163f97d99cfb4412
Igor Ivanov <igori@nvidia.com>
2022-01-13 14:18:24 +0200

    issue: 2934322 Enable CI on ci-demo
    Signed-off-by: Igor Ivanov <igori@nvidia.com>

--- tests/verifier/lib/TUL.pm
+++ tests/verifier/lib/TUL.pm
@@ -963,7 +963,7 @@ our $test_suite_tcp_ul =
                     failure => ['Segmentation fault', 'Assertion', 'ERROR']
                 },
                 client => {
-                    success => ['Test ended', 'No valid observations found'],
+                    success => ['Test ended', 'Test end before test start'],
                     failure => ['Segmentation fault', 'Assertion', 'ERROR', 'server down']
                 }
             },
